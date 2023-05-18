18c75b2400e01beedec3c4e6dd061e4a3fbc8771
myanpetra99 <myanpetra99@gmail.com>
2023-05-18 17:16:55 +0700

    test for deployment

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,22 +3,8 @@ import '../styles/App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { getLatestRelease } from '../scripts/services';
-import { useEffect, useState } from 'react';
 
 function App() {
-
-  const [latestRelease, setLatestRelease] = useState('')
- 
-useEffect(() => {
-  const fetchLatestRelease = async () => {
-    const data = await getLatestRelease();
-    setLatestRelease(data);
-  }
-
-  fetchLatestRelease();
-}, []);
-
   return (
     <>
       <h1 className='brand title-hero'>GPT-OTG</h1>
@@ -26,7 +12,7 @@ useEffect(() => {
         Ask the AI anywhere and anytime. <br />
       </p>
       <div className="card">
-        <a href='https://github.com/myanpetra99/gpt-otg/releases/latest/download/gpt-otg.crx'><button> Download The Extension {latestRelease} <FontAwesomeIcon icon={faDownload} /></button></a>
+        <a href='https://github.com/myanpetra99/gpt-otg/releases/latest/download/gpt-otg.crx'><button> Download The Extension<FontAwesomeIcon icon={faDownload} /></button></a>
         <a href='https://github.com/myanpetra99/GPT-OTG'><button> Github  <FontAwesomeIcon icon={faGithub} /> </button></a>
       </div>
     </>
